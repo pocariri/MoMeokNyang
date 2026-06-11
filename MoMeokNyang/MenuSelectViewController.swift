64cdd7699cba8fddc9e9a7fff50fb9463241c1c9
pocariri <wsdvbn011@gmail.com>
2026-06-11 19:24:52 +0900

    feat: 메뉴 선택 화면 UI 구현

--- MoMeokNyang/MenuSelectViewController.swift
+++ MoMeokNyang/MenuSelectViewController.swift
@@ -1,10 +1,3 @@
-//
-//  MenuSelectViewController.swift
-//  MoMeokNyang
-//
-//  Created by 서승혁 on 6/11/26.
-//
-
 import UIKit
 
 class MenuSelectViewController: UIViewController {
@@ -12,18 +5,9 @@ class MenuSelectViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
     }
-    */
-
 }
