00e812856c891c8efbb46ee8b5326f1c19b5e3bf
pocariri <wsdvbn011@gmail.com>
2026-06-11 22:09:56 +0900

    feat: FirebaseAuth 연동 및 로그인 기능 구현

--- MoMeokNyang/LoginViewController.swift
+++ MoMeokNyang/LoginViewController.swift
@@ -1,20 +1,23 @@
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
-    
     @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
     }
     
+    // 뒤로가기 버튼
     @IBAction func backButtonTapped(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
+    // UI 셋업
     private func setupUI() {
         // 이메일 입력창
         emailTextField.layer.cornerRadius = 15
@@ -23,5 +26,25 @@ class LoginViewController: UIViewController {
         // 비밀번호 입력창
         passwordTextField.layer.cornerRadius = 15
         passwordTextField.clipsToBounds = true
+    }
+    
+    // 로그인 버튼
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        guard let email = emailTextField.text, !email.isEmpty,
+              let password = passwordTextField.text, !password.isEmpty else {
+            print("이메일과 비밀번호를 모두 입력해주세요!")
+            return
+        }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResults, error in
+            if let error = error {
+                print("로그인 실패")
+                return
+            }
+            
+            print("로그인 성공")
+            
+            self?.dismiss(animated: true, completion: nil)
+        }
     }
 }
