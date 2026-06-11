85cd62508046815ea9c8f110942afe470c1a4db1
pocariri <wsdvbn011@gmail.com>
2026-06-11 20:46:33 +0900

    feat: 기록 및 프로필 탭과 로그인 화면 연결

--- MoMeokNyang/LoginViewController.swift
+++ MoMeokNyang/LoginViewController.swift
@@ -2,7 +2,26 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    private func setupUI() {
+        // 이메일 입력창
+        emailTextField.layer.cornerRadius = 15
+        emailTextField.clipsToBounds = true
+        
+        // 비밀번호 입력창
+        passwordTextField.layer.cornerRadius = 15
+        passwordTextField.clipsToBounds = true
     }
 }
--- MoMeokNyang/ProfileViewController.swift
+++ MoMeokNyang/ProfileViewController.swift
@@ -5,4 +5,17 @@ class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isUserLoggedIn = false
+        
+        if !isUserLoggedIn {
+            guard let loginVC = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController else { return }
+            
+            loginVC.modalPresentationStyle = .fullScreen
+            self.present(loginVC, animated: true, completion: nil)
+        }
+    }
 }
